3a6a3f38b38568755b6e942d42d927ca06d6d937
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-06-25 23:48:50 -0500

    feat: chat recupera historial

--- src/components/assistant/FAQSection.jsx
+++ src/components/assistant/FAQSection.jsx
@@ -1,7 +1,7 @@
 const FAQSection = () => {
 	return (
 		// Aplicamos un fondo con degradado de amarillo claro a oscuro, bordes redondeados y sombra
-		<section className="flex flex-col gap-4 rounded-md shadow-md p-4 bg-primary-400 text-white ">
+		<section className="flex flex-col gap-4 rounded-md shadow-md p-6 bg-gradient-to-r from-yellow-100 to-yellow-200 text-yellow-900 ">
 			{/* Encabezado estilizado con tamaño de fuente grande, negrita y color amarillo oscuro */}
 			<h2 className="text-2xl font-bold">Preguntas Frecuentes</h2>
 			{/* Lista con viñetas y espacio entre elementos, además de efectos de transición en hover */}
